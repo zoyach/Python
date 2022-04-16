cf295d2cf65d8c7f2df80883d43cf675901a4277
Zoya CH <zch00321@gmail.com>
2022-04-16 14:39:45 +0500

    Python practice

---
+++ module.py
@@ -0,0 +1,24 @@
+import math
+#Return arc cosine of a number 
+val =int(input("Enter value to find arc cosine:"))
+print(math.acos(val))
+
+# Return the inverse hyperbolic cosine of a number
+val = int(input("Enter value to find inverse hyperbolic cosine: "))
+print(math.acosh(val))
+
+# Return the arc sine of numbers
+val = int(input("Enter value to find arc sine : "))
+print(math.asin(val))
+
+# Return the inverse hyperbolic sine of numbers
+val = int(input("Enter value to find inverse hyperbolic sine: "))
+print(math.asinh(val))
+
+#Return the arc tangent of different numbers
+val= int(input("Enter a value to find arc tangent: "))
+print(math.atan(val))
+
+# Return the arc tangent of y/x in radians
+print(math.atan2(2,3))
+
