59d5cddd8776f3cee15141cfb83a5980b874adeb
Zoya CH <zch00321@gmail.com>
2022-04-17 13:04:11 +0500

    Dynamic Table using For Loop

--- table.py
+++ table.py
@@ -1,7 +1,12 @@
-a=int(input("enter table number: "))
+'''a=int(input("enter table number: "))
 b=int(input("enter the number to which table is to printed: "))
 i=1
 while i<=b:
     print(a,"x",i,"=",a*i)
-    i=i+1
+    i=i+1'''
 
+a=int(input("enter table number: "))
+b=int(input("enter the number to which table is to printed: "))
+i=1
+for i in range(i,b+1):
+     print(a,"x",i,"=",a*i)
