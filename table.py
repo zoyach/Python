4ce9b15fc28af7428331b3749d325e148f1d7b78
Zoya CH <zch00321@gmail.com>
2022-04-18 23:29:40 +0500

    Class 7 Assignment solution(list)

--- table.py
+++ table.py
@@ -1,9 +1,4 @@
-'''a=int(input("enter table number: "))
-b=int(input("enter the number to which table is to printed: "))
-i=1
-while i<=b:
-    print(a,"x",i,"=",a*i)
-    i=i+1'''
+
  
 a=int(input("enter table number: "))
 b=int(input("enter the number to which table is to printed: "))
