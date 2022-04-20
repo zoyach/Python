c4edea57130d98e6391f21f8a325c3c2b0dbb040
Zoya CH <zch00321@gmail.com>
2022-04-20 17:49:37 +0500

    even odd function

--- function.py
+++ function.py
@@ -1,16 +1,19 @@
-num = int(input("Enter a number to check Even or Odd: "))
-
-
 def find_evenodd(num):
     '''
     This function check even or odd check and return true or false respectively.
     '''
-    if (num%2==0):
-       print(num, "is an Even number")
+    if (num % 2 == 0):
+        return True
     else:
-      print(num,"is an Odd number")
-     
-find_evenodd(num)
+        return False
 
 
+num = int(input("Enter a number to check Even or Odd: "))
 
+
+answer = find_evenodd(num)
+
+if answer == True:
+    print("even number")
+else:
+    print("odd")
