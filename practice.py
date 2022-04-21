54f712a4d037708f5f0f5607afebd114732fbe3b
Zoya CH <zch00321@gmail.com>
2022-04-21 15:19:46 +0500

    Negative Indices

--- practice.py
+++ practice.py
@@ -7,3 +7,18 @@ print(name[0])
 print(name[1])
 print(name[2])
 print(name[3])
+
+#String Slicing
+# A string in Python can be sliced for getting a part of the string.
+print(name[0:3])
+print(name[:3])
+print(name[0:])
+
+''' Negative Indices:
+ Negative indices can also be used.
+  -1 corresponds to the (length-1) index, -2 to (length-2).'''
+  
+print(name[-4:-1])     #same as name[1:4]
+
+
+
